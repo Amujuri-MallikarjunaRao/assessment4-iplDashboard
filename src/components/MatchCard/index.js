fb3a92a726ade3d3b76f13a0ba584ee4e07ad330
Arjun Palvoy <133219453+Amujuri-MallikarjunaRao@users.noreply.github.com>
2023-09-14 19:29:32 +0530

    Update index.js
    match card js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,24 @@
 // Write your code here
+// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {eachM} = props
+  const {result} = eachM
+  const cLogo = eachM.competing_team_logo
+  const cTeam = eachM.competing_team
+  const alt = `competing team ${cTeam}`
+  const status = eachM.match_status
+  const rORg = status === 'Won' ? 'green' : 'red'
+
+  return (
+    <li className="card-mini">
+      <img src={cLogo} alt={alt} className="c-logo" />
+      <p className="mini-p">{cTeam}</p>
+      <p className="mini-p">{result}</p>
+      <p className={`mini-p ${rORg}`}>{status}</p>
+    </li>
+  )
+}
+
+export default MatchCard
